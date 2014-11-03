7318c6e8414dff41db5097778368621f5446dcb5
Janno Põldma <janno.poldma@gmail.com>
2014-11-03 08:04:35 +0200

    First RaphaelJS example.

--- LuckNov14/Page.fs
+++ LuckNov14/Page.fs
@@ -8,7 +8,11 @@ open System.IO
 open System.Reflection
 
 let hello () =
-    Globals.window.alert("Hello, World!")
+    let paper = Globals.Raphael.Invoke(10.0, 50.0, 320.0, 200.0)
+    let circle = paper.circle(50.0, 40.0, 10.0)
+                      .attr("fill", "#f00")
+                      .attr("stroke", "#fff")
+    ()
 
 [<LuckNov14.JS.JSMain("page.js")>]
 let main () =
--- LuckNov14/Program.fs
+++ LuckNov14/Program.fs
@@ -6,31 +6,27 @@ open Suave.Http.RequestErrors
 open Suave.Http.Successful
 open Suave.Types
 open Suave.Web
-
-printfn "tere"
+open System.Text
 
 LuckNov14.JS.compileAll()
 
-(*
-let default_mime_types_map = function
-    | ".js" -> mk_mime_type "application/x-javascript" true
-    | _     -> None
-*)
+let content = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
+    <title>LuckNov14</title>
+</head>
+<body style=""background-color: #ccc;"">
+    <script src=""https://code.jquery.com/jquery-2.1.1.min.js""></script>
+    <script src=""//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js""></script>
+    <script src=""/Scripts/page.js""></script>
+</body>
+</html>"
 
 let app : WebPart =
     choose [
-        url "/page" >>= OK """<!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>LuckNov14</title>
-            </head>
-            <body>
-                <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-                <script src="/Scripts/page.js"></script>
-            </body>
-            </html>"""
+        url "/page" >>= OK content
         GET >>= url_regex "/Scripts/(.*)\.js$" >>= browse
         NOT_FOUND "Found no handlers"
     ]
