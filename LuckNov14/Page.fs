e2af22e70f66577295a4b03aa364df12a78264ee
Janno Põldma <janno.poldma@gmail.com>
2014-12-29 11:08:39 +0200

    Animation test.

--- LuckNov14/Page.fs
+++ LuckNov14/Page.fs
@@ -9,20 +9,30 @@ open System.IO
 open System.Reflection
 
 let add key value (x: Dictionary<_,_>) =
-    x.Add(key, value)
+    x.Add(key, box value)
     x
 
-[<LuckNov14.JS.JSMain("page.js")>]
-let main () =
+[<JSEmitInline("{0}")>]
+let asFunction (f: 'a -> 'b): Function = failwith "never"
+
+type SimpleGame () =
     let preload (game: Phaser.Game) =
         game.load.image("logo", "Images/phaser.png")
 
     let create (game: Phaser.Game) =
         let logo = game.add.sprite(game.world.centerX, game.world.centerY, "logo")
-        logo.anchor.setTo(0.5, 0.5)
+
+        logo.anchor.setTo(0.5, 0.5) |> ignore
+        logo.scale.setTo(0.2, 0.2) |> ignore
+
+        let twopt = Dictionary<_, obj>() |> add "x" 1 |> add "y" 1
+        game.add.tween(logo.scale)._to(twopt, 2000.0, Phaser.Easing.Bounce.Out |> asFunction, true)
 
     let options = Dictionary<_, obj>()
-                  |> add "preload" (box preload)
-                  |> add "create" (box create)
+                  |> add "preload" preload
+                  |> add "create" create
 
-    Phaser.Game.Create(800.0, 600.0, Phaser_.AUTO, "", options)
+    let game = Phaser.Game.Create(800.0, 600.0, Phaser_.AUTO, "", options)
+
+[<LuckNov14.JS.JSMain("page.js")>]
+let main () = SimpleGame()
